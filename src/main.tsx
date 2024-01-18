2a01c7309766807de41fbb80ce0cd7e6430dd468
unknown <smdemon3@gmail.com>
2024-01-18 12:42:28 +0600

    steup redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import routers from './routes/routes.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import routers from "./routes/routes.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={routers} />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={routers} />
+    </Provider>
+  </React.StrictMode>
+);
