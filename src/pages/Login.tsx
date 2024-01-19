e6940abbfe147e404885c310ece7c8d2acb83107
unknown <smdemon3@gmail.com>
2024-01-19 19:28:20 +0600

    add middleware into baseApi and succesfully login with setup refresh token in cookies

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,32 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
+
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+
+  const [login, { data, error }] = useLoginMutation();
+
+  console.log("data => ", data);
+  console.log("error => ", error);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSubmit = (data: any) => {
+    login(data);
+  };
   return (
-    <div>
-      <h1>Hello from login form</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID: </label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="id">Password: </label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit"> Login</Button>
+    </form>
   );
 };
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
+import { baseApi } from "./api/baseApi";
 
 export const store = configureStore({
-  reducer: { auth:  authReducer },
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares().concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
