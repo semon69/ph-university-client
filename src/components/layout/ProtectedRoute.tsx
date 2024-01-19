c9dfa143bbe14d56805984fe9f8a66b176dddb2f
unknown <smdemon3@gmail.com>
2024-01-19 22:18:08 +0600

    create protected route to secure site

---
+++ src/components/layout/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import { useAppSelector } from "../../redux/hook";
+import { useCurrentToken } from "../../redux/features/auth/authSlice";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+  const token = useAppSelector(useCurrentToken);
+  if(!token){
+    return <Navigate to={'/login'} replace></Navigate>
+  }
+  return children;
+};
+
+export default ProtectedRoute;
